74d617a6798df445eca7309fea676c661bd5e4f5
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2024-11-04 14:09:25 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pastilaris: {
+          primary: "#B7EB38",
+          black: "#0B251C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
